8dab999754edea58599af3d70a89e3b440cf3d08
sabindice <sabin.bunea@gmail.com>
2021-11-06 00:12:21 +0200

    -all task done

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -3,6 +3,8 @@
 namespace App\Http\Controllers;
 
 use App\Http\Requests\ProfileUpdateRequest;
+use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 
 class ProfileController extends Controller
 {
@@ -15,6 +17,12 @@ class ProfileController extends Controller
     {
         // Task: fill in the code here to update name and email
         // Also, update the password if it is set
+
+	    $payload = $request->validated();
+		if(isset($payload['password'])){
+			$payload['password'] = Hash::make($payload['password']);
+		}
+		auth()->user()->update($payload);
 
         return redirect()->route('profile.show')->with('success', 'Profile updated.');
     }
